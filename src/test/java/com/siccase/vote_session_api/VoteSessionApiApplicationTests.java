4b0cfe086b42cd5f8ab4b775d0f00ae83478482e
isaiaszanoni <isaiasz.mus@gmail.com>
2026-04-23 23:15:23 -0300

    ci: change build.gradle to exclude classes to jacoco report

--- src/test/java/com/siccase/vote_session_api/VoteSessionApiApplicationTests.java
+++ src/test/java/com/siccase/vote_session_api/VoteSessionApiApplicationTests.java
@@ -1,8 +1,6 @@
 package com.siccase.vote_session_api;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
